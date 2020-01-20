dcf63ed76572ac5bd64d63c9b8bafaae545f6076
d-yosh <daicedaice87@gmail.com>
2020-01-20 13:37:30 +0900

    TooMuchMessageApp を実装

--- src/main/scala/TooMuchMessageApp.scala
+++ src/main/scala/TooMuchMessageApp.scala
@@ -1,3 +1,24 @@
-object TooMuchMessageApp extends App {
+import java.time.LocalDateTime
+import java.util.concurrent.TimeUnit
 
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
+
+class SlowActor extends Actor {
+  val log = Logging(context.system, this)
+  def receive = {
+    case message: String => {
+      log.info(message)
+      Thread.sleep(1000)
+    }
+    case _ =>
+  }
+}
+
+object TooMuchMessageApp extends App {
+  val system = ActorSystem("tooMuchMessageApp")
+  val slowActor = system.actorOf(Props[SlowActor], "slowActor")
+  while(true) {
+    slowActor ! LocalDateTime.now().toString
+  }
 }
