cb18fb388e2c5d875ef8fc4b0385d44b495dd2d4
d-yosh <daicedaice87@gmail.com>
2020-01-20 13:31:33 +0900

    ReplyActorApp を実装

--- src/main/scala/ReplyActorApp.scala
+++ src/main/scala/ReplyActorApp.scala
@@ -1,3 +1,19 @@
-object ReplyActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
 
+class Reply extends Actor {
+  def receive = {
+    case str if str == "aaa" =>
+      println(str)
+      sender() ! "bbb"
+    case str if str == "bbb" =>
+      println(str)
+      sender() ! "aaa"
+  }
+}
+
+object ReplyActorApp extends App {
+  val system = ActorSystem("reply")
+  val reply1 = system.actorOf(Props[Reply], "reply1")
+  val reply2 = system.actorOf(Props[Reply], "reply2")
+  reply1.tell("aaa", reply2)
 }
